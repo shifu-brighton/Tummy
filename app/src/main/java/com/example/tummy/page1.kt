4105c8314806067f4baac0d5aa71b65f95ecb052
shifu-brighton <ifayabrighton@gmail.com>
2022-11-12 14:33:15 +0300

    first page design.. can't believe its taken me half the day..lol

--- app/src/main/java/com/example/tummy/page1.kt
+++ app/src/main/java/com/example/tummy/page1.kt
@@ -1,11 +1,17 @@
 package com.example.tummy
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 
 class page1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // If the Android version is lower than Jellybean, use this call to hide
+        // the status bar.
+
         setContentView(R.layout.activity_page1)
     }
 }
